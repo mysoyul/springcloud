ac45cf8d1152445c95c9556fe42ff98ce39e2db4
mysoyul <vega2k_@naver.com>
2022-06-02 15:23:42 +0900

    cloud level1

---
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -0,0 +1,52 @@
+package com.example.secondservice;
+
+import java.util.Collections;
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/second-service")
+//@RequestMapping("/")
+@Slf4j
+public class SecondServiceController {
+	@Autowired
+	Environment env;
+	
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Second Service Request Header : " + header;
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        Enumeration<String> headers = request.getHeaderNames();
+        Collections.list(headers).stream().forEach(name -> {
+            Enumeration<String> values = request.getHeaders(name);
+            Collections.list(values).stream().forEach(value -> System.out.println("Header : " + name + "=" + value));
+        });
+
+        log.info(">> server port={}", request.getServerPort());
+
+        log.info(">> spring.cloud.client.hostname={}", env.getProperty("spring.cloud.client.hostname"));
+        log.info(">> spring.cloud.client.ip-address={}", env.getProperty("spring.cloud.client.ip-address"));
+
+        return String.format(">> Hi, there. This is a message from First Service on PORT %s"
+                , env.getProperty("local.server.port"));
+    }
+}
